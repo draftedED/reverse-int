e8e17582ddfeb24b019cc0ff64d132a48988429a
Pavel Pavlov <pavel.pavlov.edc@gmail.com>
2023-08-02 21:46:38 +0300

    feat: add a function with solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+   const inputToString = n.toString();
+   let reverseString = '';
+   for (i = 0; i < inputToString.length; i++) {
+      reverseString += inputToString[inputToString.length - i - 1]
+   }  
+   return parseInt(reverseString);
 }
